9ca8cf4869703dffd9834bdfc11f4231a9d4f3d5
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2025-07-22 14:28:41 +0530

    file routing and Link

---
+++ src/app/performance/page.tsx
@@ -0,0 +1,12 @@
+// import Navbarpage from "@/components/header";
+const PerformancePage =()=>{
+    return(
+        <div>
+           {/* <Navbarpage/> */}
+            Performance page
+        </div>
+    )
+
+}
+
+export default PerformancePage;
---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbarpage = () => {
+  return (
+    <div>
+      <nav className="container flex items-center justify-between mx-auto h-14">
+        <Link href="/">
+          <h1 className="font-bold text-xl">Home</h1>
+        </Link>
+
+        <div className="flex gap-4">
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbarpage;
